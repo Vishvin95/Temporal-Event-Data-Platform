1a0491a448242083b8e1ca41b7042547264d5fdd
Vishwesh Vinchurkar <vishwesh.vinchurkar@gmail.com>
2020-04-22 00:49:01 +0530

    [Feature-1&2 changes] - Added SQL constraints(not null and unique)

--- src/main/java/com/temporal/model/Event.java
+++ src/main/java/com/temporal/model/Event.java
@@ -1,9 +1,14 @@
 package com.temporal.model;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElements;
+
 public class Event {
 	private String name;
 	private String dataType;
 	private String type;
+	private boolean notNull;
+	private boolean unique;
 
 	public Event() {
 		
@@ -17,38 +22,65 @@ public class Event {
 	 *                 EventDataType.DATE_TIME
 	 * @param type     : Is it a single occurrence event ( EventType.SOE ) or multi occurrence event ( EventType.MOE ) 
 	 */
-	public Event(String name, EventDataType dataType, EventType type) {
+	public Event(String name, EventDataType dataType, EventType type, boolean notNull, boolean unique) {
 		this.name = name;
 		this.dataType = dataType.getEventDataType();
 		this.type = type.getEventType();
+		this.notNull = notNull;
+		this.unique = unique;
 	}
 
-	public String getname() {
+	public String getName() {
 		return name;
 	}
 
-	public void setname(String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getdataType() {
+	public String getDataType() {
 		return dataType;
 	}
 
-	public void setdataType(String dataType) {
+	public void setDataType(String dataType) {
 		this.dataType = dataType;
 	}
 
-	public String gettype() {
+	public String getType() {
 		return type;
 	}
 
-	public void settype(String type) {
+	public void setType(String type) {
 		this.type = type;
+	}
+	
+	@XmlElement(name = "notnull")
+	public boolean isNotNull() {
+		return notNull;
+	}
+
+	public void setNotNull(boolean notNull) {
+		this.notNull = notNull;
+	}
+
+	@XmlElement(name = "unique")
+	public boolean isUnique() {
+		return unique;
+	}
+
+	
+	public void setUnique(boolean unique) {
+		this.unique = unique;
 	}
 
 	@Override
 	public String toString() {
-		return "\t" + name + "\t" + dataType + "\t" + type;
+		String eventString;
+		eventString = "\t" + name + "\t" + dataType + "\t" + type;
+		if(this.notNull)
+			eventString = eventString + "\tnot_null";
+		if(this.unique)
+			eventString = eventString + "\tunique";
+		return eventString;
 	}
 }
